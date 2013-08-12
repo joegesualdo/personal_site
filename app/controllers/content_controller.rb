ca2015f6b1d035effaee57768e58511d52da9a79
Joe Gesualdo <joegesualdo@gmail.com>
2013-08-12 14:08:57 -0400

    Change resume pdf from iframe to inline

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -2,6 +2,8 @@ class ContentController < ApplicationController
 
 
   def resume
+    pdf_filename = File.join(Rails.root, "app/assets/images/gesualdo_resume.pdf")
+    send_file pdf_filename, :filename => "gesualdo_resume.pdf", :disposition => 'inline', :type => "application/pdf"
   end
 
   def home
